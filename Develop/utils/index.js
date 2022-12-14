7ce37163f1451b711f4c466d96d9610bd45b3954
Lee <leemyers16@gmail.com>
2022-12-14 15:06:06 -0800

    project start

---
+++ Develop/utils/index.js
@@ -0,0 +1,5 @@
+// TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
+const util = require('util');
